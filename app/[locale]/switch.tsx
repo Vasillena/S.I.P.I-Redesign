67221f910d6c564fbeb2fadd3e95e01888ab42f7
Vasilena Yoneva <114015792+Vasillena@users.noreply.github.com>
2024-08-16 01:17:51 +0300

    Update switch.tsx

--- app/[locale]/switch.tsx
+++ app/[locale]/switch.tsx
@@ -2,35 +2,29 @@
 
 import { useChangeLocale, useCurrentLocale } from "../../locales/client";
 
-import blackSans from "next/font/local";
-import { cn } from "@/lib/utils";
-
-const blackSansFont = blackSans({ src: "../../public/Blacker-Sans.ttf" });
+// import { cn } from "@/lib/utils";
 
 export function Switch() {
   const changeLocale = useChangeLocale();
   const locale = useCurrentLocale();
-  const myFont = blackSansFont;
 
   return (
     <>
       <button
-        className={cn(
-          "w-[50px] h=[35px] drop-shadow-sm text-xl bg-[#F7F4F1]",
-          myFont.className,
-          locale === "en" && "hidden"
-        )}
+        className="w-[50px] h=[35px] drop-shadow-sm text-xl bg-[#F7F4F1]"
+        // myFont.className,
+        // locale === "en" && "hidden"
+
         type="button"
         onClick={() => changeLocale("en")}
       >
         EN
       </button>
       <button
-        className={cn(
-          "w-[50px] h=[35px] drop-shadow-sm text-xl bg-[#F7F4F1]",
-          myFont.className,
-          locale === "bg" && "hidden"
-        )}
+        className="w-[50px] h=[35px] drop-shadow-sm text-xl bg-[#F7F4F1]"
+        // myFont.className,
+        // locale === "bg" && "hidden"
+
         type="button"
         onClick={() => changeLocale("bg")}
       >
